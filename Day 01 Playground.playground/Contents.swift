db349ea21f2c196b2671e8fdd0889410422bb8b0
Casssie <cassieareff@gmail.com>
2019-07-08 14:51:13 -0700

    initial commit

---
+++ Day 01 Playground.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+var str = "Hello, playground"
+//var age = "17"
+//"I am \(age) years old"
+let string1 = "hello"
+let string2 = "there"
+var welcome = "\(string1) \(string2)"
+var anotherWelcome = string1 + " " + string2
+print ("Hello World")
+var first = "glossier"
+var last = "queen"
+print ("EmilyWeiss")
+print("\(first) \(last)")
+print("\(last) \(first) \(first) \(last)")
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+a+b*c/d
+
+let name = "Amelie"
+var address = "Glossier Canyon"
+let birthday = "May 22, 2002"
+var gradeLevel = "11th"
+var age = "17"
+let eyeColor = "brown"
+var favoriteColor = "Glossier pink"
+
+var StringtoCompress = "Angeles"
+print("\(StringtoCompress.first!) \(StringtoCompress.count-2)\(StringtoCompress.last!)")
+
+5 < 8
+12 > 7
+6 != 8
+7 == 7
+
+"karlie" == "karlie"
+"karlie" == "karlie"
+"Karlie" == "Karlie"
+
+var luckyNum = 7
+luckyNum < 10
+luckyNum == 7
+
+var dogAge = 1
+if dogAge < 2 {
+    print("You are a puppy 🐶")
+}
+
+if dogAge == 2 {
+    print("You are awesome!")
+}
+
+if dogAge == 3 {
+    print("You are awesome!")
+}
+
+if dogAge == 4 {
+    print("You are awesome!")
+}
+
+// etc.
+
+if dogAge > 12 {
+    print("You are elderly 💗")
+}
+
+var favoriteFood = "strawberries"
+if favoriteFood == "Chipotle" {
+print ("Ew Chipotle is gross")
+}
+else if favoriteFood == "jamba juice" {
+    print ("I love Jamba Juice too!")
+}
+else if favoriteFood == "i dont know" {
+print ("that's okay")
+}
+else {
+    print("Regardless of what you like, youre awesome")
+}
+
